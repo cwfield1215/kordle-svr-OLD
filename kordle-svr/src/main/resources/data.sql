0ac5c09c983b8d044302a81cd7b5e0f7416d8daa
cwfield1215 <103618457+cwfield1215@users.noreply.github.com>
2022-05-02 15:00:01 -0400

    Initial Commit

---
+++ kordle-svr/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO KORDLE_RESULT VALUES (0, 'chrfield', 1000, 3, 'solved');    
+INSERT INTO KORDLE_RESULT VALUES (1, 'rjfield', 3400, 4, 'solved');    
+INSERT INTO KORDLE_RESULT VALUES (2, 'ethelpk', 1000, 2, 'solved');    
